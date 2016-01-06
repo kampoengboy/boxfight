41bbb3fc516d181f33aeeceecb7ce7455472bbff
Michael <mike_visualsoft@yahoo.com>
2016-01-06 21:14:16 +0700

    add attack

--- app/src/main/java/com/delapan3gp/boxfight/MainActivity.java
+++ app/src/main/java/com/delapan3gp/boxfight/MainActivity.java
@@ -1,11 +1,14 @@
 package com.delapan3gp.boxfight;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     public static float right_p2 = 0;
     public static int click_left = 0;
     public static int click_right = 0;
+    public static int hp_player1 = 200;
+    public static int hp_player2 = 200;
     public static ImageView imgview;
     public static ImageView imgview2;
     public static Button btn_attack_player1;
@@ -28,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         final RelativeLayout.LayoutParams rel_btn = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         final Button btn_left_player1 = (Button)findViewById(R.id.btn_left_player1);
         final Button btn_left_player2 = (Button)findViewById(R.id.btn_left_player2);
-        Button btn_right_player1 = (Button)findViewById(R.id.btn_right_player1);
-        Button btn_right_player2 = (Button)findViewById(R.id.btn_right_player2);
+        final Button btn_right_player1 = (Button)findViewById(R.id.btn_right_player1);
+        final Button btn_right_player2 = (Button)findViewById(R.id.btn_right_player2);
         imgview = (ImageView)findViewById(R.id.imageView);
         imgview.setImageResource(R.drawable.boxing);
         imgview2 = (ImageView)findViewById(R.id.imageView2);
@@ -38,6 +43,74 @@ public class MainActivity extends AppCompatActivity {
         final Button btn_player2 = (Button)findViewById(R.id.btn_player2);
         btn_attack_player1 = (Button)findViewById(R.id.btn_attack_player1);
         btn_attack_player2 = (Button)findViewById(R.id.btn_attack_player2);
+        btn_attack_player1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(click_left+click_right>=16)
+                {
+                    hp_player2-=50;
+                    if(hp_player2<=0){
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setMessage("Player 1 Win");
+                        builder1.setCancelable(true);
+
+                        builder1.setPositiveButton(
+                                "CLOSE",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        builder1.show();
+                        btn_left_player1.setEnabled(false);
+                        btn_left_player2.setEnabled(false);
+                        btn_right_player1.setEnabled(false);
+                        btn_right_player2.setEnabled(false);
+                        btn_attack_player1.setEnabled(false);
+                        btn_attack_player2.setEnabled(false);
+                        ProgressBar pgBar = (ProgressBar)findViewById(R.id.progressBar2);
+                        pgBar.setProgress(0);
+                    } else {
+                        ProgressBar pgBar = (ProgressBar) findViewById(R.id.progressBar2);
+                        pgBar.setProgress(hp_player2);
+                    }
+                }
+            }
+        });
+        btn_attack_player2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(click_left+click_right>=16)
+                {
+                    hp_player1-=50;
+                    if(hp_player1<=0){
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setMessage("Player 2 Win");
+                        builder1.setCancelable(true);
+
+                        builder1.setPositiveButton(
+                                "CLOSE",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        builder1.show();
+                        btn_left_player1.setEnabled(false);
+                        btn_left_player2.setEnabled(false);
+                        btn_right_player1.setEnabled(false);
+                        btn_right_player2.setEnabled(false);
+                        btn_attack_player1.setEnabled(false);
+                        btn_attack_player2.setEnabled(false);
+                        ProgressBar pgBar = (ProgressBar)findViewById(R.id.progressBar);
+                        pgBar.setProgress(0);
+                    } else {
+                        ProgressBar pgBar = (ProgressBar) findViewById(R.id.progressBar);
+                        pgBar.setProgress(hp_player1);
+                    }
+                }
+            }
+        });
         //btn_player1.setLayoutParams(rel_btn);
         btn_left_player1.setOnClickListener(new View.OnClickListener() {
             @Override
